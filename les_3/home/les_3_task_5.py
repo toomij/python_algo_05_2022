ce9b39ac4a0987fbb7c97876dc3521d2737e31a1
Artem Tovbin <arty99@gmail.com>
2022-05-23 18:38:34 +0300

    Refactor task 5

--- les_3/home/les_3_task_5.py
+++ les_3/home/les_3_task_5.py
@@ -1,14 +1,31 @@
 # 5. В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
+import random
 
+SIZE = 10
+array = [random.randint(-100, 100) for _ in range(SIZE)]
+print(array)
 
-def MaxElem(list1):
-    maxelement = list1[0]
-    for element in list1:
-        if element > maxelement:
-            maxelement = element
-    return maxelement
+i = 0
+index = -1
 
+while i < SIZE:
+    if array[i] < 0 and index == -1:
+        index = i
+    elif 0 > array[i] > array[index]:
+        index = i
 
-l=[-5, -6, -5, -60, -32 ]
-result = MaxElem(l)
-print(result)
+    i += 1
+
+print(f'Число {array[index]} на позиции {index}')
+
+# def MaxElem(list1):
+#     maxelement = list1[0]
+#     for element in list1:
+#         if element > maxelement:
+#             maxelement = element
+#     return maxelement
+#
+#
+# l=[-5, -6, -5, -60, -32 ]
+# result = MaxElem(l)
+# print(result)
