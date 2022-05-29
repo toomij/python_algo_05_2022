2f1f60b5b9069048194f8108d5a8ef6922f284b6
Artem Tovbin <arty99@gmail.com>
2022-05-29 12:13:46 +0300

    Refactoring prime() func

--- les_4/home/les_4_task_1_2.py
+++ les_4/home/les_4_task_1_2.py
@@ -1,3 +1,4 @@
+#  -*- coding: utf-8 -*-
 # 5. В массиве найти максимальный отрицательный элемент.
 # Вывести на экран его значение и позицию в массиве.
 
@@ -19,4 +20,13 @@ def max_below_zero(size):
 
         i += 1
 
-    return f'Число {array[index]} на позиции {index}'
+    #return f'Число {array[index]} на позиции {index}'
+    return array[index]
+
+
+# python -m timeit -n 1000 -s "import les_4_task_1_2" "les_4_task_1_2.max_below_zero(5)"
+# 1000 loops, best of 3: 7.64 usec per loop
+# python -m timeit -n 1000 -s "import les_4_task_1_2" "les_4_task_1_2.max_below_zero(10)"
+# 1000 loops, best of 3: 14.7 usec per loop
+# python -m timeit -n 1000 -s "import les_4_task_1_2" "les_4_task_1_2.max_below_zero(100)"
+# 1000 loops, best of 3: 136 usec per loop
